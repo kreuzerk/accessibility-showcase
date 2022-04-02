0c2d479832f559b925f97907833300caee6c0ca3
kreuzerk <kevin.kreuzer90@icloud.com>
2022-04-02 15:50:44 +0200

    feat: 🎸 (heading) add heading component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   declarations: [
-    SidenavComponent
+    SidenavComponent,
+    HeadingComponent
   ],
-  exports: [SidenavComponent],
+  exports: [SidenavComponent, HeadingComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/core/heading/heading.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.scss']
+})
+export class HeadingComponent {
+  @Input() heading: string | undefined;
+
+}
